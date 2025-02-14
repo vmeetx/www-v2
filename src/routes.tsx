7f2ae8d41b5c70aaea8f170799952d687262a085
sa-fw-an <isafwansayeed@gmail.com>
2025-02-14 15:49:38 +0530

    Fixing the GH-Page deployement error

--- src/routes.tsx
+++ src/routes.tsx
@@ -15,22 +15,29 @@ import Volunteer from "./pages/Volunteer";
 import Donate from "./pages/Donate";
 import Products from "./pages/Products";
 
-const router = createBrowserRouter([
-  { path: "/", element: <MainPage /> },
-  { path: "/about-us", element: <AboutUs /> },
-  { path: "/mission", element: <Mission /> },
-  { path: "/leadership", element: <Leadership /> },
-  { path: "/contact-us", element: <ContactUs /> },
-  { path: "/faqs", element: <FAQs /> },
-  { path: "/community-news", element: <CommunityNews /> },
-  { path: "/events", element: <Events /> },
-  { path: "/press-release", element: <PressRelease /> },
-  { path: "/sugar-stories", element: <SugarStories /> },
-  { path: "/try-sugar", element: <TrySugar /> },
-  { path: "/join-development", element: <JoinDevelopment /> },
-  { path: "/volunteer", element: <Volunteer /> },
-  { path: "/donate", element: <Donate /> },
-  { path: "/products", element: <Products /> },
-]);
+const BASE_PATH = '/www-v2';
+
+const router = createBrowserRouter(
+  [
+    { path: `/`, element: <MainPage /> },
+    { path: `/about-us`, element: <AboutUs /> },
+    { path: `/mission`, element: <Mission /> },
+    { path: `/leadership`, element: <Leadership /> },
+    { path: `/contact-us`, element: <ContactUs /> },
+    { path: `/faqs`, element: <FAQs /> },
+    { path: `/community-news`, element: <CommunityNews /> },
+    { path: `/events`, element: <Events /> },
+    { path: `/press-release`, element: <PressRelease /> },
+    { path: `/sugar-stories`, element: <SugarStories /> },
+    { path: `/try-sugar`, element: <TrySugar /> },
+    { path: `/join-development`, element: <JoinDevelopment /> },
+    { path: `/volunteer`, element: <Volunteer /> },
+    { path: `/donate`, element: <Donate /> },
+    { path: `/products`, element: <Products /> },
+  ],
+  {
+    basename: BASE_PATH, // Set the basename to match the repository name
+  }
+);
 
 export default router;
