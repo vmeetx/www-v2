5f162105030839b74c91a509b21f362084c7862b
Shubham Singh <shubhsoch@gmail.com>
2025-03-27 17:44:22 +0530

    SVGs and NAVIGATION (#78)
    * SVGs changed
    
    * navites to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 
 const App = () => {
+  useEffect(() => {
+    const unsubscribe = router.subscribe(() => {
+      window.scrollTo(0, 0);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col">
       <RouterProvider router={router} />
