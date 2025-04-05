c2fee6433c48ab9c06b985bbcfc41def1c9908f7
Safwan Sayeed <isafwansayeed@gmail.com>
2025-04-05 10:27:51 +0530

    Fix routing logic

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,17 @@ const App = () => {
     const unsubscribe = router.subscribe(() => {
       window.scrollTo(0, 0);
     });
+    const handleRedirect = () => {
+      const redirectPath = sessionStorage.getItem('gh_redirect');
+      if (redirectPath) {
+        console.log('Restoring route:', redirectPath);
+        sessionStorage.removeItem('gh_redirect');
+        setTimeout(() => {
+          router.navigate(redirectPath);
+        }, 10);
+      }
+    };
+    handleRedirect();
 
     return () => unsubscribe();
   }, []);
