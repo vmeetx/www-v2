a64ac3ad8a6f1b00885fff7b6af36692897078c7
Safwan Sayeed <isafwansayeed@gmail.com>
2025-04-06 17:53:56 +0530

    Changed import route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
-import router from './routes';
+import router from '@/routes';
 
 const App = () => {
   useEffect(() => {
