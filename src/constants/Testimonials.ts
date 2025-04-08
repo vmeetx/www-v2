b063bfb2b944d9e41116cb8a9a46cfce31c5cc84
Bekarys Serikov <49273199+Beka01247@users.noreply.github.com>
2025-04-08 08:36:15 +0500

    feat: add real data to words of appreciation (#103)
    * feat: add real data to words of appreciation
    
    * fix: changed the format

--- src/constants/Testimonials.ts
+++ src/constants/Testimonials.ts
@@ -30,4 +30,34 @@ export const testimonials = [
     body: 'Fedora-Based Sugar on a Stick Is One Sweet Desktop.',
     img: 'https://avatar.vercel.sh/james',
   },
+  {
+    name: 'Harshit Verma',
+    username: 'Harshit Verma',
+    body: 'Contribution to Sugar Labs has been an incredible rewarding experience. It has not only helped me grow as a developer but also given me confidence to work on real world projects. I’m excited about what’s next in my open source journey, and I can’t wait to continue learning and contributing!',
+    img: 'https://miro.medium.com/v2/resize:fill:44:44/1*devzHlZAKt4fVu5Cg0eN6w.png',
+  },
+  {
+    name: 'Om Santosh Suneri',
+    username: 'Om Santosh Suneri',
+    body: 'My first open source contribution was challenging but incredibly rewarding. If you’re hesitant about contributing, just dive in. Find a project you love, pick an issue, and start exploring.Who knows? Your first PR might be just around the corner.',
+    img: 'https://miro.medium.com/v2/da:true/resize:fill:44:44/0*975x0N6-dYtP01Y7',
+  },
+  {
+    name: 'Vipul Gupta',
+    username: 'Vipul Gupta',
+    body: 'Sugar Labs excelled all my expectations of how an open-source community would be like. The work I did during the course of the program would sure benefit the community. But more importantly, from day one, I felt to be working for something much bigger than myself.',
+    img: 'https://miro.medium.com/v2/resize:fill:44:44/1*QK0Aauitgk6kBBB6XpX98A.jpeg',
+  },
+  {
+    name: 'Sam Parkinson',
+    username: 'Sam Parkinson, Google Code-in grand prize winner',
+    body: "GCI was truly something that changed my life. I went from being an open source newbie to being able to contribute to really cool projects, thanks to the amazing GCI and Sugar Labs communities. It's something that I would recommend any young programmer consider doing. Participating in GCI is something that can make dreams come true.",
+    img: 'https://avatar.vercel.sh/james',
+  },
+  {
+    name: 'Riya J',
+    username: 'Riya J. GSoC 2023 Student',
+    body: 'I learned a lot during the whole summer especially about backward compatibility when I worked on Sugar, which is a free/libre open-source software learning platform for children. I would like to thank my mentors, Chihurumnaya Ibiam and Sourabha Ganesh who provided constant support whenever I faced any issue. Looking forward to contributing more to Open Source!',
+    img: 'https://media.licdn.com/dms/image/v2/D5603AQGfK0K2z8gjMg/profile-displayphoto-shrink_100_100/B56ZR54oTdHwAY-/0/1737211676388?e=1749686400&v=beta&t=Kk7H2ex_3Vzmuw69QRiR55ZuXftrd4n60xRUFTbeDGo',
+  },
 ];
