3d5276613c7d40f92c630d277e6496463a696361
Safwan Sayeed <isafwansayeed@gmail.com>
2025-04-23 10:57:25 +0530

    Update Not Found page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -29,10 +29,18 @@ const NotFoundPage: React.FC = () => {
           initial="hidden"
           animate="visible"
           variants={fadeIn}
-          className="relative py-20 md:py-28 overflow-hidden bg-gradient-to-b from-black via-gray-800 to-white"
+          className="relative py-5 md:py-15 overflow-hidden bg-gradient-to-b from-black via-gray-800 to-white"
         >
           <div className="absolute inset-0 z-0 overflow-hidden">
-            <div className="absolute inset-0 bg-[url('/images/matrix-bg-pattern.svg')] opacity-10"></div>
+            <div className="absolute inset-0 opacity-10"></div>
+            <div
+              className="absolute inset-0"
+              style={{
+                backgroundImage:
+                  'radial-gradient(rgba(255, 255, 255, 0.1) 1px, transparent 1px)',
+                backgroundSize: '20px 20px',
+              }}
+            ></div>
           </div>
           <div className="container mx-auto px-4 md:px-6">
             <div className="flex flex-col items-center text-center z-10 relative">
